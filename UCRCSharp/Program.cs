1d48ba4e56c672f1546813ff5b9b6f0824fde2e4
erezrob <erez.robinson@gmail.com>
2012-12-25 13:04:32 +0200

    Primary Commit

---
+++ UCRCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UCRCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UCR.DTW(args[0], args[1], int.Parse(args[2]), float.Parse(args[3]));
+        }
+    }
+}
